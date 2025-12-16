52bd0e7c6f0082189499c48ee5d01a72cec2898c
ANMOL K <anmolk@ANMOLs-MacBook-Air.local>
2025-12-17 00:47:51 +0530

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import {create} from 'zustand'
+
+const useMacBookStore = create((set)=>({
+    color:'#2e2e2e',
+    setColor:(color)=> set({color}),
+
+    scale: 0.08,
+    setScale:(scale)=> set({scale}),
+
+    texture: "/videos/feature-1.mp4",
+    setTexture: (texture) => set({texture}),
+    reset: ()=> set({color:'#2e2c3e', sclae:0.08, texture:'/videos/feature-1.mp4'}),
+}))
+
+export default useMacBookStore;
